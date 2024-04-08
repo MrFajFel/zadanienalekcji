4e1e2d24afe28723c121af784a238f15143094c3
Student <elkadjk@.pl>
2024-04-08 10:26:34 +0200

    pierwszy

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Adres adres1 = new Adres("ulica","123wow","50-223","Wroclaw");
-        Adres adres2 = new Adres("jakas","21wow","50-223","Wroclaw");
+        Adres adres1 = new Adres("","123wow","50-223","Wroclaw");
+        Adres adres2 = new Adres("jakas","","56-213","Gdansk");
+        Adres adres3 = new Adres("jakas","32abc","","Szczecin");
+        Adres adres4 = new Adres("jakas","32abc","70-216","");
+
     }
 }
